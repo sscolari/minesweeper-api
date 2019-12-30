943b5d030312cd45bbc187eb0746709d583dad67
santiago.scolari <santiagosco@gmail.com>
2019-12-30 13:04:53 -0300

    contact info to swagger conf

--- src/main/java/com/deviget/minesweeper/SpringFoxConfig.java
+++ src/main/java/com/deviget/minesweeper/SpringFoxConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -15,8 +19,19 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
           .select()                                  
-          .apis(RequestHandlerSelectors.any())
+          .apis(RequestHandlerSelectors.basePackage("com.deviget.minesweeper.controller"))
           .paths(PathSelectors.any())
-          .build();                                           
+          .build()
+          .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Minesweeper API",
+                "RESTful API for the Minesweeper game by Santiago Scolari",
+                "1.0",
+                "Terms of service",
+                new Contact("Santiago Scolari", null, "santiagosco@gmail.com"),
+                null, null, Collections.emptyList());
     }
 }
